dbf230b8a2ddc959419cc9374ad890e8d2f074d8
adorable-otter <plan.stack@gmail.com>
2024-12-11 14:57:38 +0900

    feat: 각 페이지로 이동할 수 있는 네비게이션을 가지는 헤더 추가

---
+++ src/components/Header.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link';
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="text-center p-4 bg-gray-800 text-white font-bold">
+      <nav className="flex ">
+        <Link className="w-1/4" href={'/'}>
+          홈
+        </Link>
+        <Link className="w-1/4" href={'/champions'}>
+          챔피언 목록
+        </Link>
+        <Link className="w-1/4" href={'/items'}>
+          아이템 목록
+        </Link>
+        <Link className="w-1/4" href={'/rotations'}>
+          챔피언 로테이션
+        </Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
