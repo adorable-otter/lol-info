708172e5f10655b7f5c3933543f734ea3d8dadc8
adorable-otter <plan.stack@gmail.com>
2024-12-13 22:52:07 +0900

    refactor: 불필요한 파라미터 제거

--- src/app/api/rotation/route.ts
+++ src/app/api/rotation/route.ts
@@ -1,20 +1,20 @@
 import { fetchChampions } from '@/api/fetchChampions';
 import { NextResponse } from 'next/server';
 
-export async function GET(request: Request) {
-  const [rotationIds, champions] = await Promise.all([fetchRotations(request), fetchChampions()]);
+export async function GET() {
+  const [rotationIds, champions] = await Promise.all([fetchRotations(), fetchChampions()]);
   const rotationChampions = rotationIds.map((id) =>
     champions.find((champion) => Number(champion.key) === id)
   );
   return NextResponse.json({ rotationChampions });
 }
 
-const fetchRotations = async (request: Request): Promise<number[]> => {
+const fetchRotations = async (): Promise<number[]> => {
   try {
     const response = await fetch(
       'https://kr.api.riotgames.com/lol/platform/v3/champion-rotations',
       {
-        headers: { ...request.headers, 'X-Riot-Token': process.env.RIOT_API_KEY },
+        headers: { 'X-Riot-Token': process.env.RIOT_API_KEY! },
       }
     );
     const data = await response.json();
