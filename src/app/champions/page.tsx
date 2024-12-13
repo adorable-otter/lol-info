2ca5044c12282cd52190ba87f5b73daf669ef9ee
adorable-otter <plan.stack@gmail.com>
2024-12-13 11:59:01 +0900

    design: 기본 테마를 다크로 임시 변경

--- src/app/champions/page.tsx
+++ src/app/champions/page.tsx
@@ -7,7 +7,7 @@ const ChampionsPage = async () => {
   const champions = await fetchChampions();
 
   return (
-    <div className='flex flex-wrap gap-5 justify-center bg-black p-10'>
+    <div className="flex flex-wrap gap-5 justify-center">
       {champions.map((champion) => (
         <ChampionCard key={champion.id} champion={champion} />
       ))}
