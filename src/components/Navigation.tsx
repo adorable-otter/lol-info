ebb93adfaa94eebba1a0236f5309070575a3b348
adorable-otter <plan.stack@gmail.com>
2024-12-18 20:57:35 +0900

    fix: 모달에 있는 메뉴 문구 줄 바꿈 되지 않게 수정

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -12,16 +12,16 @@ const Navigation = () => {
   };
   return (
     <nav className="flex flex-col sm:flex-row items-center gap-5 text-center w-full">
-      <Link onClick={handleLinkClick} className="w-1/4" href={'/'}>
+      <Link onClick={handleLinkClick} className="w-full sm:w-1/4" href={'/'}>
         홈
       </Link>
-      <Link onClick={handleLinkClick} className="w-1/4" href={'/champions'}>
+      <Link onClick={handleLinkClick} className="w-full sm:w-1/4" href={'/champions'}>
         챔피언 목록
       </Link>
-      <Link onClick={handleLinkClick} className="w-1/4" href={'/items'}>
+      <Link onClick={handleLinkClick} className="w-full sm:w-1/4" href={'/items'}>
         아이템 목록
       </Link>
-      <Link onClick={handleLinkClick} className="w-1/4" href={'/rotation'}>
+      <Link onClick={handleLinkClick} className="w-full sm:w-1/4" href={'/rotation'}>
         챔피언 로테이션
       </Link>
     </nav>
